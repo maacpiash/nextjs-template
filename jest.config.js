4756fdf5d151a842a0a2fea4dc3bdec03c0f8f05
Ahad Chowdhury <ahad@maacpiash.com>
2021-04-21 13:20:14 +1000

    Update Jest config for dac492d

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,6 @@ module.exports = {
   ],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/tests/__mocks__/fileMock.js',
   },
 }
